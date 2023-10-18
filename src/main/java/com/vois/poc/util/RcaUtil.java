4cedb5a74a19fa77e7147497012dcc90a084a24a
rachit1707 <rachitsharma2020@gmail.com>
2023-10-18 11:34:48 +0530

    Adding BPMN Fix

--- src/main/java/com/vois/poc/util/RcaUtil.java
+++ src/main/java/com/vois/poc/util/RcaUtil.java
@@ -19,10 +19,10 @@ public class RcaUtil {
         rcaData.put("PPB03733290","Issue with the phone");
         rcaData.put("PRB0003764","Please ask the agent to update the date");
         rcaData.put("211989057","Please ask the agent to update the phone number for the customer");
-        rcaData.put("490279977","The phone number provided with the customer is not verified");
+        rcaData.put("490279977","Order has reached the point of no return hence cannot be cancelled");
         rcaData.put("12222","The role is not found in the Database please add role before providing the access");
         rcaData.put("455666","The user does not have access to the UAT");
-        rcaData.put("123456","Fix for the issue raised");
+        rcaData.put("123456","Deployment Scheduled");
         rcaData.put("1222112","Automation Test scripts were running which led to un-availability of the resources");
         rcaData.put("5465665","User password has expired");
         rcaData.put("233444","Major fix release is provided");
